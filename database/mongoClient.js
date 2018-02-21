26c59d7100f751e3bf229c1054dae3f294afdf45
Samir Goel <samirgoel3@gmail.com>
2018-02-21 17:24:41 +0530

    change in  mongo client

--- database/mongoClient.js
+++ database/mongoClient.js
@@ -2,7 +2,7 @@
 
   var mongoose = require('mongoose');
   mongoose.Promise = global.Promise;
-  mongoose.connect('mongodb://localhost:27017/samirdatabase');
+  mongoose.connect('mongodb://heroku_8t6df7pn:bcdc924jbldp2cuubsesn9ol0@ds127132.mlab.com:27132/heroku_8t6df7pn');
 
   // var Todo = mongoose.model('Mdata', {
   //     text:{type: String, required:true, minLength:1, trim:true},
