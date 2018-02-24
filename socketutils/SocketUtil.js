6643a9e352d13034919ae2032450c76026172ab5
Samir Goel <samirgoel3@gmail.com>
2018-02-24 18:04:52 +0530

    a

--- socketutils/SocketUtil.js
+++ socketutils/SocketUtil.js
@@ -21,7 +21,7 @@ function insideInjector(unique_no){
             console.log("Unable to send data on the selected unique no");
         }else{
             console.log("sending data to particular device:"+doc[0].socket_id);
-            console.log("List of connected sockets :"+io.sockets.clients());
+            console.log("List of connected sockets :"+io.sockets.clients().length);
             // io.sockets.connected[doc[0].socket_id].emit("app_data", ""+event.target.injector_data);
         }
     } , (err)=>{
