09cb214df183f629be173b8490004bbca063bf46
Samir Goel <samirgoel3@gmail.com>
2018-02-24 11:20:11 +0530

    de

--- socketutils/SocketUtil.js
+++ socketutils/SocketUtil.js
@@ -22,8 +22,7 @@ function insideInjector(unique_no){
             console.log("Unable to send data on the selected unique no");
         }else{
             console.log("sending data to particular device:"+doc[0].socket_id);
-            // io.sockets.connected[doc[0].socket_id].emit("app_data", ""+event.target.injector_data);
-            io.emit('app_data' , "some data uiuiuiiu");
+            io.sockets.connected[""+doc[0].socket_id].emit("app_data", ""+event.target.injector_data);
         }
     } , (err)=>{
         console.log("ERROR in finding the socket_id with respective unique no:"+err);
