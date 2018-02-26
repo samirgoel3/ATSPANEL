cd2f1d24c2f06be84ae221ab834fb2476131d537
Samir Goel <samirgoel3@gmail.com>
2018-02-26 11:46:31 +0530

    dot removed

--- apis/adminApis.js
+++ apis/adminApis.js
@@ -106,7 +106,7 @@ adminApiRoute.get('/removeInjectorById' , (req , res)=>{
 
 
 
-adminApiRoute.get('./removeAllInjectors',(req , res)=>{
+adminApiRoute.get('/removeAllInjectors',(req , res)=>{
     databaseUtils.removeAllInjectors().then((doc)=>{
         if(doc == null || doc == undefined || doc.length == 0){
             res.send(failureResponse("It seemes like you have already cleared the table"));
